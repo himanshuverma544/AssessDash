48a8da8b9f9551c4211792fb1c58b00c43881488
Himanshu Verma <himanshuverma544@gmail.com>
2023-07-23 18:31:52 +0530

    upd: used mui component for normalizing the css

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App.jsx';
+
+import './index.css';
+import { CssBaseline } from '@mui/material';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App/>,
+  <>
+    <CssBaseline/>
+    <App/>
+  </>,
 )
