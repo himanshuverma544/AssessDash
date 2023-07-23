e9a044cfa1c870b8940ad15e619c6a33553b1007
Himanshu Verma <himanshuverma544@gmail.com>
2023-07-23 18:38:35 +0530

    add: added 404 page

---
+++ src/pages/NoPage.jsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+const NoPage = () => {
+  return (
+    <div>
+      <Typography variant="h1">404</Typography>
+    </div>
+  );
+}
+
+export default NoPage;
