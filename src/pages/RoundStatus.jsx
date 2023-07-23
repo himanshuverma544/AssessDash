d4ebbe8b1e8e5211a87b33bfad8ceb36d2b1645b
Himanshu Verma <himanshuverma544@gmail.com>
2023-07-23 18:38:21 +0530

    add: added round status page

---
+++ src/pages/RoundStatus.jsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+const RoundStatus = () => {
+   return (
+    	<Typography variant="h1">Round Status</Typography>
+   );
+}
+
+export default RoundStatus;
