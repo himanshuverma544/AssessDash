8293bc6696cf89939321530e5048a24937ebcfd6
Himanshu Verma <himanshuverma544@gmail.com>
2023-07-23 18:37:38 +0530

    add: added admin page

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material"
+
+const Admin = () => {
+
+  return (
+    <div>
+      <Typography variant="h1">Admin</Typography>
+    </div>
+  ); 
+}
+
+export default Admin;
